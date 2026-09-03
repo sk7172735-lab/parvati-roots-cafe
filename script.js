7d0a244d73b78a4a41cb40c595af700061d86585
sk7172735-lab <sk7172735@gmail.com>
2026-09-02 20:08:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// =========================
+// MOBILE MENU
+// =========================
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+
+menuToggle.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
